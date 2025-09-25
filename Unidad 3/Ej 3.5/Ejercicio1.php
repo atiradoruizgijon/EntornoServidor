723ce18064f9b7131eb149fc8f4a1a6aaa7e57f1
atiradoruizgijon <alejandro.tirado-nieto@iesruizgijon.com>
2025-09-25 20:26:31 +0200

    Empiezo ejercicio 3.7

---
+++ Unidad 3/Ej 3.5/Ejercicio1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>El resultado de la multiplicación es: <?= $_REQUEST['num1'] * $_REQUEST['num2'] ?></p>
+</body>
+</html>
---
+++ Unidad 3/Ej 3.7/Ejercicio1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cilindro</title>
+    <style>
+        *{
+            font-family: sans-serif;
+            text-align: center;
+        }
+        img{
+            width: 200px;
+            height: auto;
+        }
+        p{
+            font-size: 1.6em;
+        }
+    </style>
+</head>
+<body>
+    <h2>Cálculo del volumen de un cilindro.</h2>
+    <img src="imagenes/cilindro.png" alt="cilindro">
+    <?php
+     // Lo almaceno todo en una variable para que no sea muy largo.
+     // V = PI*r*r*altura
+     $radio = ($_REQUEST['diametro'] / 2);
+     $volumen = $_REQUEST['altura'] * $radio * $radio * M_PI; 
+    ?>
+    <p>El volumen del cilindro es de <?= $volumen ?></p>
+</body>
+</html>

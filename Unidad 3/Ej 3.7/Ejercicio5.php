5b5a7d2b3f386c69f660aa25d94c265802a7fb17
atiradoruizgijon <alejandro.tirado-nieto@iesruizgijon.com>
2025-09-26 14:33:11 +0200

    Ejercicio 5 hecho

---
+++ Unidad 3/Ej 3.7/Ejercicio5.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cilindro</title>
+    <style>
+        *{
+            font-family: sans-serif;
+            text-align: center;
+        }
+        p{
+            font-size: 1.6em;
+        }
+    </style>
+</head>
+<body>
+    <?php
+     // Lo almaceno todo en una variable para que no sea muy largo.
+     // V = PI*r*r*altura
+     $radio = ($_REQUEST['diametro'] / 2);
+     $volumen = $_REQUEST['altura'] * $radio * $radio * M_PI;
+     // 1 L = 1000 cm3
+     $min = ($volumen / 1000) * $_REQUEST['caudal'];
+     $horas = $min / 60;
+     $horas = intval($horas);
+     $min = $min % 60;
+    ?>
+    <p>El volumen del cilindro es de <?= $volumen ?></p>
+    <p>Lo que tardará en llenarse el cilindro será: <?= $horas ?> horas y <?= $min ?> minutos</p>
+</body>
+</html>

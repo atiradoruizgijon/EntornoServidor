7eac0e8f4a68d7960911b295359fb231afa21eb3
atiradoruizgijon <alejandro.tirado-nieto@iesruizgijon.com>
2025-10-10 13:59:02 +0200

    Ejercicio 1 practica array hecho

---
+++ Unidad 5/EjerPractica/5.6/Ejercicio1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        td, tr{
+            border: solid black 1px;
+            text-align: center;
+            font-size: 2rem;
+        }
+        tr:nth-child(even){
+            background-color: lightgrey;
+        }
+        table{
+            margin: auto;
+        }
+        </style>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>Numero</td>
+            <td>Cuadrado</td>
+            <td>Cubo</td>
+        </tr>
+        <?php
+      for ($i = 0; $i < 20; $i++) { 
+        $numero[$i] = rand(0, 100);
+        $cuadrado[$i] = $numero[$i] * $numero[$i];
+        $cubo[$i] = $numero[$i] * $numero[$i] * $numero[$i];
+
+        echo "<tr><td>$numero[$i]</td><td>$cuadrado[$i]</td><td>$cubo[$i]</td></tr>";
+      }
+    ?>
+    </table>
+</body>
+</html>

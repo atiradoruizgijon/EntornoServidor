18ca63f8ac6a1e74aba65816e56c655d86e4770d
atiradoruizgijon <alejandro.tirado-nieto@iesruizgijon.com>
2025-11-09 14:09:10 +0100

    Correción ejercicio 2

--- Unidad 7/Boletin/Ejercicio2.php
+++ Unidad 7/Boletin/Ejercicio2.php
@@ -27,13 +27,21 @@
         for ($i = 0; $i < $array[$indice]; $i++) {
             // icono svg con el que muestro los votos
             echo '
-                <svg style="color: ' . $color . ';" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-award-fill" viewBox="0 0 16 16">
-                    <path d="m8 0 1.669.864 1.858.282.842 1.68 1.337 1.32L13.4 6l.306 1.854-1.337 1.32-.842 1.68-1.858.282L8 12l-1.669-.864-1.858-.282-.842-1.68-1.337-1.32L2.6 6l-.306-1.854 1.337-1.32.842-1.68L6.331.864z"/>
-                    <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1z"/>
-                </svg>
-                ';
+                    <svg style="color: ' . $color . ';" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-award-fill" viewBox="0 0 16 16">
+                        <path d="m8 0 1.669.864 1.858.282.842 1.68 1.337 1.32L13.4 6l.306 1.854-1.337 1.32-.842 1.68-1.858.282L8 12l-1.669-.864-1.858-.282-.842-1.68-1.337-1.32L2.6 6l-.306-1.854 1.337-1.32.842-1.68L6.331.864z"/>
+                        <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1z"/>
+                    </svg>
+                    ';
         }
     }
+
+    if (isset($_REQUEST['SI'])) {
+        $votos[0]++;
+    }
+    if (isset($_REQUEST['NO'])) {
+        $votos[1]++;
+    }
+    setcookie("votos", base64_encode(serialize($votos)), strtotime("+3 months"));
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -54,21 +62,14 @@
     <div>
         <h4>SI</h4>
         <?php
-        if (isset($_REQUEST['SI'])) {
-            $votos[0]++;
-        }
-        if (isset($_REQUEST['NO'])) {
-            $votos[1]++;
-        }
 
         mostrarVotos($votos, 0, "green");
         echo "<hr><h4>NO</h4>";
         mostrarVotos($votos, 1, "red");
-        
-        setcookie("votos", base64_encode(serialize($votos)), strtotime("+3 months"));
+
         ?>
     </div>
-    
+
     <form action="" method="post">
         <input type="hidden" name="SI" value="">
         <input type="submit" value="SI">
